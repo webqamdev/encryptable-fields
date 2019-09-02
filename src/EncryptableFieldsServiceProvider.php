3925cf1123d7b828584529f45171b42f02c40dd3
Thomas <tcombe@webqam.fr>
2019-09-02 09:01:49 +0200

    Clean Service provider commented lines

--- src/EncryptableFieldsServiceProvider.php
+++ src/EncryptableFieldsServiceProvider.php
@@ -12,36 +12,10 @@ class EncryptableFieldsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'encryptable-fields');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'encryptable-fields');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('encryptable-fields.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/encryptable-fields'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/encryptable-fields'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/encryptable-fields'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
