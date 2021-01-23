c76c6140166c8afb71d7a6a52d3540b1776061b1
Jérémy ARGOUD <jargoud@webqam.fr>
2021-01-23 18:53:20 +0100

    Add DB support class

--- src/Http/Controllers/Admin/Traits/EncryptedSearchTrait.php
+++ src/Http/Controllers/Admin/Traits/EncryptedSearchTrait.php
@@ -3,7 +3,7 @@
 namespace Webqamdev\EncryptableFields\Http\Controllers\Admin\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
+use Webqamdev\EncryptableFields\Support\DB;
 
 trait EncryptedSearchTrait
 {
@@ -21,22 +21,13 @@ trait EncryptedSearchTrait
 
         foreach ($columns as $column) {
             $query->orWhere(
-                DB::raw($this->formatClause($column)),
+                DB::getRawClause($column),
                 'like',
                 '%' . $searchTerm . '%'
             );
         }
 
         return $query;
-    }
-
-    protected function formatClause(string $column): string
-    {
-        $format = 'convert(aes_decrypt(from_base64(json_value(from_base64(%s), "$.value")), "%s") USING utf8mb4)';
-        $escapedColumn = sprintf('`%s`', str_replace('.', '`.`', $column));
-        $key = app()->get('databaseEncrypter')->getKey();
-
-        return sprintf($format, $escapedColumn, $key);
     }
 
     protected function encryptedOrderLogic(Builder $query, $columns, string $columnDirection): Builder
@@ -47,7 +38,7 @@ trait EncryptedSearchTrait
 
         foreach ($columns as $column) {
             $query->orderBy(
-                DB::raw($this->formatClause($column)),
+                DB::getRawClause($column),
                 $columnDirection
             );
         }
