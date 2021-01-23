5e33c97b8ab75cc8f210f24ed97134cf88dabccc
Jérémy ARGOUD <jargoud@webqam.fr>
2021-01-23 16:23:23 +0100

    Clean KeyGenerateCommand.php

--- src/Console/KeyGenerateCommand.php
+++ src/Console/KeyGenerateCommand.php
@@ -3,10 +3,14 @@
 namespace Webqamdev\EncryptableFields\Console;
 
 use Illuminate\Encryption\Encrypter;
+use Illuminate\Foundation\Console\KeyGenerateCommand as BaseCommand;
 use Webqamdev\EncryptableFields\Encryption\DatabaseEncrypter;
 
-class KeyGenerateCommand extends \Illuminate\Foundation\Console\KeyGenerateCommand
+class KeyGenerateCommand extends BaseCommand
 {
+    protected const ENV_VARIABLE_NAME = 'APP_DB_ENCRYPTION_KEY';
+    protected const CONFIG_KEY = 'encryptable-fields.key';
+
     /**
      * The name and signature of the console command.
      *
@@ -26,17 +30,17 @@ class KeyGenerateCommand extends \Illuminate\Foundation\Console\KeyGenerateComma
         $key = $this->generateRandomKey();
 
         if ($this->option('show')) {
-            return $this->line('<comment>'.$key.'</comment>');
+            return $this->line('<comment>' . $key . '</comment>');
         }
 
         // Next, we will replace the application key in the environment file so it is
         // automatically setup for this developer. This key gets generated using a
         // secure random byte generator and is later base64 encoded for storage.
-        if (! $this->setKeyInEnvironmentFile($key)) {
+        if (!$this->setKeyInEnvironmentFile($key)) {
             return;
         }
 
-        $this->laravel['config']['encryptable-fields.key'] = $key;
+        $this->laravel['config'][self::CONFIG_KEY] = $key;
 
         $this->info('Application key set successfully.');
     }
@@ -48,22 +52,20 @@ class KeyGenerateCommand extends \Illuminate\Foundation\Console\KeyGenerateComma
      */
     protected function generateRandomKey()
     {
-        return 'base64:'.base64_encode(
-                Encrypter::generateKey(DatabaseEncrypter::CIPHER)
-            );
+        return 'base64:' . base64_encode(Encrypter::generateKey(DatabaseEncrypter::CIPHER));
     }
 
     /**
      * Set the application key in the environment file.
      *
-     * @param  string  $key
+     * @param string $key
      * @return bool
      */
     protected function setKeyInEnvironmentFile($key)
     {
-        $currentKey = $this->laravel['config']['encryptable-fields.key'];
+        $currentKey = $this->laravel['config'][self::CONFIG_KEY];
 
-        if (strlen($currentKey) !== 0 && (! $this->confirmToProceed())) {
+        if (strlen($currentKey) !== 0 && (!$this->confirmToProceed())) {
             return false;
         }
 
@@ -79,23 +81,28 @@ class KeyGenerateCommand extends \Illuminate\Foundation\Console\KeyGenerateComma
      */
     protected function keyReplacementPattern()
     {
-        $escaped = preg_quote('='.$this->laravel['config']['encryptable-fields.key'], '/');
+        $escaped = preg_quote('=' . $this->laravel['config'][self::CONFIG_KEY], '/');
 
-        return "/^APP_DB_ENCRYPTION_KEY{$escaped}/m";
+        return "/^" . self::ENV_VARIABLE_NAME . "{$escaped}/m";
     }
 
     /**
      * Write a new environment file with the given key.
      *
-     * @param  string  $key
+     * @param string $key
      * @return void
      */
     protected function writeNewEnvironmentFileWith($key)
     {
-        file_put_contents($this->laravel->environmentFilePath(), preg_replace(
-            $this->keyReplacementPattern(),
-            'APP_DB_ENCRYPTION_KEY='.$key,
-            file_get_contents($this->laravel->environmentFilePath())
-        ));
+        $environmentFilePath = $this->laravel->environmentFilePath();
+
+        file_put_contents(
+            $environmentFilePath,
+            preg_replace(
+                $this->keyReplacementPattern(),
+                self::ENV_VARIABLE_NAME . '=' . $key,
+                file_get_contents($environmentFilePath)
+            )
+        );
     }
 }
