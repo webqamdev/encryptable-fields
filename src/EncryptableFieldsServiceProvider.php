2506b3ec8963f5b1e30d0423774efcfed3d2787a
Jérémy ARGOUD <jargoud@webqam.fr>
2021-01-23 16:21:58 +0100

    Merge service providers

--- src/EncryptableFieldsServiceProvider.php
+++ src/EncryptableFieldsServiceProvider.php
@@ -4,6 +4,7 @@ namespace Webqamdev\EncryptableFields;
 
 use Illuminate\Support\ServiceProvider;
 use Webqamdev\EncryptableFields\Console\KeyGenerateCommand;
+use Webqamdev\EncryptableFields\Encryption\DatabaseEncrypter;
 use Webqamdev\EncryptableFields\Services\EncryptionInterface;
 
 class EncryptableFieldsServiceProvider extends ServiceProvider
@@ -14,10 +15,20 @@ class EncryptableFieldsServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('encryptable-fields.php'),
-            ], 'config');
+            $this->publishConfiguration();
         }
+    }
+
+    protected function publishConfiguration(): self
+    {
+        $this->publishes(
+            [
+                __DIR__ . '/../config/config.php' => config_path('encryptable-fields.php'),
+            ],
+            'config'
+        );
+
+        return $this;
     }
 
     /**
@@ -25,11 +36,63 @@ class EncryptableFieldsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'encryptable-fields');
-        $this->app->bind(EncryptionInterface::class, config('encryptable-fields.encryption'));
+        $this
+            ->mergeConfiguration()
+            ->registerBindings()
+            ->registerCommands()
+            ->registerDatabaseEncrypter();
+    }
+
+    protected function registerDatabaseEncrypter(): self
+    {
+        $this->app->singleton('databaseEncrypter', function ($app) {
+            $config = $app->make('config')->get('encryptable-fields');
+
+            return new DatabaseEncrypter($this->key($config));
+        });
+
+        return $this;
+    }
+
+    /**
+     * Extract the encryption key from the given configuration.
+     *
+     * @param  array  $config
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected function key(array $config)
+    {
+        return tap($config['key'], function ($key) {
+            if (empty($key)) {
+                throw new RuntimeException(
+                    'No application encryption key has been specified.'
+                );
+            }
+        });
+    }
+
+    protected function registerCommands(): self
+    {
         $this->commands([
             KeyGenerateCommand::class,
         ]);
+
+        return $this;
+    }
+
+    protected function registerBindings(): self
+    {
+        $this->app->bind(EncryptionInterface::class, config('encryptable-fields.encryption'));
+
+        return $this;
+    }
+
+    protected function mergeConfiguration(): self
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'encryptable-fields');
+
+        return $this;
     }
 }
