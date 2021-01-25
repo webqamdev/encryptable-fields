2d6842aa36d95d35925ae1eeb12076f382539090
Jérémy ARGOUD <jargoud@webqam.fr>
2021-01-25 10:18:39 +0100

    Revert "Update configuration to use DatabaseEncryption service"
    This reverts commit 88493716

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'key' => env('APP_DB_ENCRYPTION_KEY'),
-    // Need to implement Webqamdev\EncryptableFields\Services\EncryptionInterface
-    'encryption' => Webqamdev\EncryptableFields\Services\DatabaseEncryption::class,
-    'hash_salt' => '--mDwt\k+PY,}vUJf2WeYUJ]yb(7A?>>bu7fGZrDpRUn#-kab',
+    // Need to implement EncryptionInterface
+    'encryption' => \Webqamdev\EncryptableFields\Services\Encryption::class,
+    'hash_salt' => '--mDwt\k+PY,}vUJf2WeYUJ]yb(7A?>>bu7fGZrDpRUn#-kab'
 ];
