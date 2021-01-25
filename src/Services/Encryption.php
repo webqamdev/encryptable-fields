9e391d18c376e0705d3bbb77f2f09bd2b694c00f
Jérémy ARGOUD <jargoud@webqam.fr>
2021-01-25 10:18:35 +0100

    Revert "Delete deprecated Encryption service"
    This reverts commit ef743b25

---
+++ src/Services/Encryption.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Webqamdev\EncryptableFields\Services;
+
+class Encryption implements EncryptionInterface
+{
+    /**
+     * Encrypt the given value.
+     *
+     * @param  mixed $value
+     * @return string
+     */
+    public static function encrypt($value): string
+    {
+        return encrypt($value);
+    }
+
+    /**
+     * Decrypt the given value.
+     *
+     * @param  string $value
+     * @return mixed
+     */
+    public static function decrypt(string $value)
+    {
+        return decrypt($value);
+    }
+}
