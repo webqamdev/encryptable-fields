17cf009a435cef85ab00618414664df7e5217629
Robin DESORME <rdesorme@webqam.fr>
2024-12-19 15:12:48 +0100

    feat(model): add scopes to Encryptable interface

--- src/Models/Interfaces/Encryptable.php
+++ src/Models/Interfaces/Encryptable.php
@@ -27,4 +27,16 @@ interface Encryptable
     public function setEncryptedAttribute(string $key, $value): self;
 
     public function getEncryptedAttribute(string $key): mixed;
+
+    public function scopeWhereHashed(Builder $query, string $key, string $value): void;
+
+    public function scopeOrWhereHashed(Builder $query, string $key, string $value): void;
+
+    public function scopeWhereHashedNot(Builder $query, string $key, string $value): void;
+
+    public function scopeWhereEncrypted(Builder $query, string $key, string $value): void;
+
+    public function scopeOrWhereEncrypted(Builder $query, string $key, string $value): void;
+
+    public function scopeWhereEncryptedNot(Builder $query, string $key, string $value): void;
 }
